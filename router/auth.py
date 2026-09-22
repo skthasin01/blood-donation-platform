1d086a69d949b3bb35c0760ee48fd141a39f2f2a
Sheikh Thasin Rahman Shiplu <88823794+skthasin01@users.noreply.github.com>
2026-09-22 22:16:53 +0600

    Update get current user

--- router/auth.py
+++ router/auth.py
@@ -35,18 +35,24 @@ def create_access_token(username: str,user_id : int,role : str, expires_delta:ti
     encode.update({'exp': expires})
     return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
 
-def get_current_user(token: Annotated[str,Depends(Oauth2_bearer)]):
+
+def get_current_user(token: Annotated[str, Depends(Oauth2_bearer)]):
     try:
-        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
-        username:str = payload.get('sub')
-        user_id:int = payload.get('id')
-        role : str = payload.get('role')
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM] )
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        role: str = payload.get("role")
+
         if username is None or user_id is None:
             raise HTTPException(status_code=401,detail="Could not validate credentials")
-        return {'username':username,'id': user_id, 'role' : role}
-    except JWTError:
-        raise HTTPException(status_code=401,detail="Could not validate credentials")
-    
+        return {
+            "username": username,
+            "id": user_id,
+            "role": role
+        }
+    except JWTError as e:
+        raise HTTPException(status_code=401,detail=f"JWT Error: {str(e)}")
+
 
 
 def get_db():
