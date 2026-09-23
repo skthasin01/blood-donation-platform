3562a9819500b7be4493c6b959aa7fbc32b8610a
Sheikh Thasin Rahman Shiplu <88823794+skthasin01@users.noreply.github.com>
2026-09-23 08:42:16 +0600

    Update blood_request.py

--- router/blood_request.py
+++ router/blood_request.py
@@ -133,8 +133,6 @@ def get_specific_blood_request(user: user_dependency,db : db_dependency):
         raise HTTPException(status_code=401, detail="Failed Authentication")
 
     blood_r = db.query(BloodRequest).filter(BloodRequest.user_id == user['id']).all()
-    if blood_r is None:
-        raise HTTPException(status_code=404,detail="Blood request not found")
     return blood_r
 
 @router.put('/blood-request/update/{request_id}')
