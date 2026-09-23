65a6f4e938593c0632d885ff9a419a5d488864e4
Sheikh Thasin Rahman Shiplu <88823794+skthasin01@users.noreply.github.com>
2026-09-23 22:01:09 +0600

    adding netlify link

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173" 
+        "http://localhost:5173",
+        "https://blood-donation-platfrom.netlify.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
